6dd62b57d441a43d7a2c28eb968c7909766a9b47
jiaheng.song <song8713@qq.com>
2021-09-17 23:36:38 +0800

    结构建立

---
+++ AnnotationTranslateMain.js
@@ -0,0 +1,24 @@
+
+
+var translate = function(text) {
+
+}
+
+var dealWithFile = function(filePath, dealCall) {
+    //找到所有的注释行和注释块
+    let contentText = "";
+
+    // contentText.replace("//(.*?)\n", )
+}
+
+var forEachFiles = function (rootPath, dealCall) {
+
+}
+
+var main = function () {
+    let rootPath = "";
+    forEachFiles(rootPath, dealWithFile);
+}
+
+//入口
+main();
