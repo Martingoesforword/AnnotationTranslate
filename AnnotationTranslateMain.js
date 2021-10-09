d8b7776bde809df009b947484d40f08999a37442
jiaheng.song <jiaheng.song@whaty.com>
2021-10-09 16:46:52 +0800

    基本成型：需要修复多批次翻译出现的axios错误：通过延时解决，还需要解决字符串匹配长度过大的问题

--- AnnotationTranslateMain.js
+++ AnnotationTranslateMain.js
@@ -1,6 +1,10 @@
 var util = require('util');
 const axios = require('axios')
-const CFG_URL = "http://trans.api.martinsong.org";
+const fs = require('fs');
+const path=require('path');
+
+
+const CFG_URL = "http://127.0.0.1:9991";
 
 
 //example
@@ -11,42 +15,34 @@ const CFG_URL = "http://trans.api.martinsong.org";
 // "replaced":true,
 // "cached":true}'
 var buildRequest = function (texts) {
-    var json_data = JSON.stringify({
+    var json_data = {
         url:"",
         source: texts,
         trans_type: "en2zh",
         page_id: 144200,
         replaced: true,
         cached: true
-    })
+    };
     return json_data;
 }
 
 var parseRespones = function (res) {
-    var zh_data = res.data.target;
+    var zh_data = [];
+    res.data.target.forEach((item)=>zh_data.push(item.target.slice(26)));
     return zh_data;
 }
 
-requestMyTencent = async function(data) {
-    var ret = axios.post(CFG_URL, data);
-    return ret;
-}
-
-var tencentTransZh = function (texts) {
-    var json_data = buildRequest(texts);
-    var respones_data =  requestMyTencent(json_data);
-    respones_data.then(function (res) {
-        var zh_data = parseRespones(res);
-        console.log(zh_data);
+var translate = async function(texts, callback) {
+    var data = buildRequest(texts);
+    let str = JSON.stringify(data);
+    let response = await axios({
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        url: CFG_URL,
+        data: str
     })
-}
-
-
-var translate = function(texts) {
-    //构建腾讯翻译接口数据json
-    //zh_data是和texts长度一样的数组
-    var zh_data = tencentTransZh(texts);
-    return zh_data;
+    var zh_data = parseRespones(response);
+    console.log(zh_data)
 }
 
 //逻辑：/* //一样高
@@ -54,32 +50,33 @@ var translate = function(texts) {
 //    //.*\n
 //   /\*(.*)\*/|//(.*)\n
 
-var dealWithFile = function(filePath, dealCall) {
-
-    let fileContent = "";
+var dealWithFile = function(filePath) {
+    const fileContent = fs.readFileSync(filePath).toString();
     //找到所有的注释行和注释块
     const regexp = RegExp("(/\*(.*)\\*/)|(//(.*)\\n)",'g');
     let results = fileContent.matchAll(regexp);
     let texts = Array.from(results, m => m[0]);
 
+    if(!texts.length) return;
+
     //翻译块
-    let zh_arr = translate(texts);
+    translate(texts, function (zh_arr){
+        //替换%s
+        const regexp2 = RegExp("//(.*)\\n",'g');
+        let content = fileContent.replace(regexp2, "%s\n");
+        const regexp1 = RegExp("/\*(.*)\\*/",'g');
+        content = content.replace(regexp1, "%s");
+        if(texts.length !== zh_arr.length){
+            console.log("翻译结果不一致");
+        }
 
-    //替换%s
-    const regexp2 = RegExp("//(.*)\\n",'g');
-    let content = fileContent.replace(regexp2, "%s\n");
-    const regexp1 = RegExp("/\*(.*)\\*/",'g');
-    content = content.replace(regexp1, "%s");
-    if(texts.length !== zh_arr.length){
-        console.log("翻译结果不一致");
-    }
+        //填充翻译结果
+        var result_content = util.format(content, ...texts);
+        console.log(result_content);
 
-    //填充翻译结果
-    var result_content = util.format(content, ...texts);
-    console.log(result_content);
+        //写入文件
 
-    //写入文件
-
+    })
 }
 
 //遍历目录下的所有文件
@@ -89,28 +86,24 @@ let matchSuffixes = {
     ".h":1,
     ".hpp":1
 }
-var forEachFiles = function (rootPath) {
-    let curPathNode = rootPath.getAllNode();
 
-    curPathNode.forEach(function (node) {
-        if(node.isFile())
-        {
-            //文件的话获取后缀判断属不属于需要替换的文件
-            var suffix = "";
-            if(matchSuffixes[suffix])
-            {
-                dealWithFile(node.path);
-            }
-        }
-        else if(node.isFolder())
-        {
-            forEachFiles(node.path);
+var travel = function (dir){
+    fs.readdirSync(dir).forEach((file)=>{
+        var pathname=path.join(dir,file);
+        if(fs.statSync(pathname).isDirectory()){
+            travel(pathname);
+        }else if(matchSuffixes[path.extname(pathname)]){
+            dealWithFile(pathname);
         }
     })
 }
 
+var forEachFiles =  function (rootPath) {
+     travel(rootPath);
+}
+
 var main = function () {
-    let rootPath = "";
+    let rootPath = "D:\\workplace\\cpp\\RuiKeStd_Soui2.x-master\\RuiKeStd\\aa\\";
     forEachFiles(rootPath);
     //完成，MD，记一次肚子疼写代码的经历
 }
@@ -120,6 +113,6 @@ var test = function () {
 }
 
 //入口
-//main();
+main();
 
-test();
+//test();
