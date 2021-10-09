0c1cd353813a774d9b127549973a2142691695a2
jiaheng.song <jiaheng.song@whaty.com>
2021-10-09 19:09:20 +0800

    修复axios同步冲突问题，修复翻译语言api调用问题，修复正则表达式的错误问题 ，修复代码中存在%s等的问题                    ！！！已知问题：无法快速的异步调用翻译接口，还没有实现翻译结果文件替换的功能，字符串中出现/*,//的时候也会被翻译导致错误

--- AnnotationTranslateMain.js
+++ AnnotationTranslateMain.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const path=require('path');
 
 
+
+// const CFG_URL = "http://trans.api.martinsong.org";
 const CFG_URL = "http://127.0.0.1:9991";
 
 
@@ -14,10 +16,13 @@ const CFG_URL = "http://127.0.0.1:9991";
 // "page_id":144200,
 // "replaced":true,
 // "cached":true}'
+
+// 构建请求body数据对象
 var buildRequest = function (texts) {
     var json_data = {
         url:"",
         source: texts,
+        fromlang: "en",
         trans_type: "en2zh",
         page_id: 144200,
         replaced: true,
@@ -26,57 +31,76 @@ var buildRequest = function (texts) {
     return json_data;
 }
 
-var parseRespones = function (res) {
+// 解析翻译api结果数据
+var parseReceiveInfo = function (res) {
     var zh_data = [];
-    res.data.target.forEach((item)=>zh_data.push(item.target.slice(26)));
+    res.data.target.forEach((item)=>zh_data.push(item.target.slice(24)));
     return zh_data;
 }
 
-var translate = async function(texts, callback) {
+//翻译过程
+var translateTenApi = async function(texts) {
     var data = buildRequest(texts);
-    let str = JSON.stringify(data);
     let response = await axios({
+        url: CFG_URL,
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        url: CFG_URL,
-        data: str
+        data:  JSON.stringify(data)
     })
-    var zh_data = parseRespones(response);
-    console.log(zh_data)
+    var zh_data = parseReceiveInfo(response);
+    return zh_data;
 }
 
 //逻辑：/* //一样高
 //    /\*([\n.]*)\*/
 //    //.*\n
 //   /\*(.*)\*/|//(.*)\n
-
-var dealWithFile = function(filePath) {
-    const fileContent = fs.readFileSync(filePath).toString();
+var dealWithFile = async function(filePath) {
+    var fileContent = fs.readFileSync(filePath).toString();
     //找到所有的注释行和注释块
-    const regexp = RegExp("(/\*(.*)\\*/)|(//(.*)\\n)",'g');
+    const regexp = RegExp("/\\*(([\\s\\S\\n])*?)\\*/|//(.*)",'g');
     let results = fileContent.matchAll(regexp);
-    let texts = Array.from(results, m => m[0]);
+    let texts = Array.from(results, m => m[1] || m[3]);
 
     if(!texts.length) return;
 
+    // let curCount = 0;
+    // let curArr = [];
+    // while (curCount < )
+
     //翻译块
-    translate(texts, function (zh_arr){
-        //替换%s
-        const regexp2 = RegExp("//(.*)\\n",'g');
-        let content = fileContent.replace(regexp2, "%s\n");
-        const regexp1 = RegExp("/\*(.*)\\*/",'g');
-        content = content.replace(regexp1, "%s");
-        if(texts.length !== zh_arr.length){
-            console.log("翻译结果不一致");
-        }
+    let zh_arr = await translateTenApi(texts);
 
-        //填充翻译结果
-        var result_content = util.format(content, ...texts);
-        console.log(result_content);
+    //备份已有的%s, %d, %f等为MfNlHt35wvkv43hhe-s, MfNlHt35wvkv43hhe-d, MfNlHt35wvkv43hhe-f
+    const regexp_s = RegExp("%s",'g');
+    const regexp_d = RegExp("%d",'g');
+    const regexp_f = RegExp("%f",'g');
+    let content = fileContent.replace(regexp_s, "MfNlHt35wvkv43hhe-s");
+    content = content.replace(regexp_d, "MfNlHt35wvkv43hhe-d");
+    content = content.replace(regexp_f, "MfNlHt35wvkv43hhe-f");
 
-        //写入文件
+    //替换%s
+    const regexp2 = RegExp("//(.*)",'g');
+    content = content.replace(regexp2, "//%s");
+    const regexp1 = RegExp("/\\*(([\\s\\S\\n])*?)\\*/",'g');
+    content = content.replace(regexp1, "/*%s*/");
+    if(texts.length !== zh_arr.length){
+        console.log("翻译结果不一致");
+    }
 
-    })
+
+
+    //填充翻译结果
+    content = util.format(content, ...zh_arr);
+
+    //恢复已有的%s, %d, %f等为MfNlHt35wvkv43hhe-s, MfNlHt35wvkv43hhe-d, MfNlHt35wvkv43hhe-f
+    content = content.replace("MfNlHt35wvkv43hhe-s", "%s");
+    content = content.replace("MfNlHt35wvkv43hhe-d", "%d");
+    content = content.replace("MfNlHt35wvkv43hhe-f", "%f");
+
+    console.log(content);
+    //写入文件
+
 }
 
 //遍历目录下的所有文件
@@ -87,24 +111,21 @@ let matchSuffixes = {
     ".hpp":1
 }
 
-var travel = function (dir){
-    fs.readdirSync(dir).forEach((file)=>{
+var forEachFiles = async function (dir){
+    let alldir = fs.readdirSync(dir);
+    for (var file of alldir) {
         var pathname=path.join(dir,file);
         if(fs.statSync(pathname).isDirectory()){
-            travel(pathname);
+            await forEachFiles(pathname);
         }else if(matchSuffixes[path.extname(pathname)]){
-            dealWithFile(pathname);
+            await dealWithFile(pathname);
         }
-    })
+    }
 }
 
-var forEachFiles =  function (rootPath) {
-     travel(rootPath);
-}
-
-var main = function () {
-    let rootPath = "D:\\workplace\\cpp\\RuiKeStd_Soui2.x-master\\RuiKeStd\\aa\\";
-    forEachFiles(rootPath);
+var main = async function () {
+    let rootPath = "D:\\workplace\\cpp\\RuiKeStd_Soui2.x-master\\RuiKeStd";
+    await forEachFiles(rootPath);
     //完成，MD，记一次肚子疼写代码的经历
 }
 
