bf902f775d5954a8b48e2e1734470e3042573e2a
jiaheng.song <song8713@qq.com>
2021-10-21 22:30:34 +0800

    优化翻译接口调用

--- AnnotationTranslateMain.js
+++ AnnotationTranslateMain.js
@@ -5,8 +5,8 @@ const path=require('path');
 
 
 
-//const CFG_URL = "http://trans.api.martinsong.org";
-const CFG_URL = "http://127.0.0.1:9992";
+const CFG_URL = "http://trans.api.martinsong.org";
+//const CFG_URL = "http://127.0.0.1:9992";
 
 
 //example
@@ -40,16 +40,44 @@ var parseReceiveInfo = function (res) {
 
 //翻译过程
 var translateTenApi = async function(texts) {
-    var data = buildRequest(texts);
-    let response = await axios({
-        url: CFG_URL,
-        method: "POST",
-        timeout: 5000,
-        headers: { "Content-Type": "application/json" },
-        data:  JSON.stringify(data),
-    })
-    var zh_data = parseReceiveInfo(response);
-    return zh_data;
+    let allGroups = [];
+
+    let curGroup = [];
+
+    let curCharCount = 0;
+    for(var i=0,len=texts.length; i<len; i+=1){
+        let curText = texts[i];
+        curCharCount += texts.length;
+        if(curCharCount > 5000)
+        {
+            var data = buildRequest(curGroup);
+            curCharCount = 0;
+            curGroup = [];
+            let response = await axios({
+                url: CFG_URL,
+                method: "POST",
+                timeout: 5000,
+                headers: { "Content-Type": "application/json" },
+                data:  JSON.stringify(data),
+            })
+            var zh_data = parseReceiveInfo(response);
+            allGroups = allGroups.concat(zh_data);
+        }
+        curGroup.push(curText);
+    }
+    if(curGroup.length) {
+        data = buildRequest(curGroup);
+        let response = await axios({
+            url: CFG_URL,
+            method: "POST",
+            timeout: 5000,
+            headers: { "Content-Type": "application/json" },
+            data:  JSON.stringify(data),
+        })
+        zh_data = parseReceiveInfo(response);
+        allGroups = allGroups.concat(zh_data);
+    }
+    return allGroups;
 }
 
 //逻辑：/* //一样高
@@ -59,7 +87,8 @@ var translateTenApi = async function(texts) {
 var dealWithFile = async function(filePath) {
     var fileContent = fs.readFileSync(filePath).toString();
     //找到所有的注释行和注释块
-    let results = fileContent.matchAll(/\/\*(([\s\S\n])*?)\*\/|\/(.*)/g);
+    const regexp = RegExp("/\\*(([\\s\\S\\n])*?)\\*/|//(.*)",'g');
+    let results = fileContent.matchAll(regexp);
     let texts = Array.from(results, m => m[1] || m[3]);
 
     if(!texts.length) return;
@@ -128,6 +157,7 @@ var dealWithFile = async function(filePath) {
 //遍历目录下的所有文件
 let matchSuffixes = {
     ".c":1,
+    ".cc":1,
     ".cpp":1,
     ".h":1,
     ".js":1,
@@ -144,7 +174,7 @@ var forEachFiles = function (dir){
         var pathname=path.join(dir,file);
         if(fs.statSync(pathname).isDirectory()){
             forEachFiles(pathname);
-        }else if(matchSuffixes[path.extname(pathname)]){
+        }else if(matchSuffixes[path.extname(pathname)] && pathname.indexOf("node_modules") === -1){
             allFiles.push(pathname);
         }
     }
@@ -163,7 +193,7 @@ var dealForEachFiles = async function (){
 
 
 var main = async function () {
-    let rootPath = "D:\\workplace\\cpp\\SSS_operating_system\\30days_REF";
+    let rootPath = "D:\\workplace\\cpp\\electron\\electron";
     forEachFiles(rootPath);
     await dealForEachFiles();
     //完成，MD，记一次肚子疼写代码的经历
